1cfb567d7281fbc2bb528af9a34ec6d504dfbc1f
Jessica Swenson <jessicamswens@gmail.com>
2023-01-24 18:07:45 -0700

    started responsive landing page

---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => {
+
+    return (
+        <div className='landing-page-main-div'>
+            <div className='name-circle'>
+                <h1>JESSICA SWENSON</h1>
+                <h3 className='web-dev-h3'>web developer</h3>
+            </div>
+            <div className='scroll-landing-div'>
+                <p className='scroll-landing-p'>scroll</p>
+                <a href=""><div className='arrow down'></div></a>
+                <a href=""><div className='arrow down'></div></a>
+                <a href=""><div className='arrow down'></div></a>
+            </div>
+        </div>
+    );
+}
+
+export default LandingPage;
