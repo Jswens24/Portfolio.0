606ddcc65c179ae5b92438217eaf637afe81daf5
Jessica Swenson <jessicamswens@gmail.com>
2023-02-09 18:10:16 -0700

    updated alt text

--- src/Components/AboutPage.jsx
+++ src/Components/AboutPage.jsx
@@ -8,7 +8,7 @@ const AboutPage = () => {
             <div className='gold-box-div'>
                 <div className='about-me-pic-div'>
                     <h2 className='about-me-h2'>ABOUT ME</h2>
-                    <img className='portfolio-pic' src='https://drive.google.com/uc?export=view&id=1Vqvp_DNooIELcVbju_wRweselPFDuOwT' alt='picture of me!' />
+                    <img className='portfolio-pic' src='https://drive.google.com/uc?export=view&id=1Vqvp_DNooIELcVbju_wRweselPFDuOwT' alt='me!' />
                 </div>
                 <div className="bio-education-div">
                     <p className='about-me-bio'>Hi, I’m Jessica!  I’m from Salt Lake City, and I’m a Web Developer.  I am also a serial learner, whether it's learning a new skill, hobby, or sport, I am always learning something new.  After working in finance and then getting a degree in Business Administration.  I decided to take a Coding Basics class and I immediately fell in love with programming.  I love the challenge and creativity that it offers.  I attended Devmountain's Web Development program where I learned the foundations of the web as well as specialized in React. </p>
