295f6c7011c084831a8c17ff3ad586fd408e713d
Jessica Swenson <jessicamswens@gmail.com>
2023-09-28 16:25:51 -0600

    added clickable computer on landing page, added labels for devicons

--- src/Components/AboutPage.jsx
+++ src/Components/AboutPage.jsx
@@ -19,22 +19,70 @@ const AboutPage = () => {
                     <div className="education-div">
                         <h3>SKILLS</h3>
                         <div className="skills-div">
-                            <img alt='react' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
-                            <img alt='html' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-                            <img alt='css' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
-                            <img alt='tailwind' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" />
-                            <img alt='javascript' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                            <img alt='node' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" />
-                            <img alt='postgres' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
-                            <img alt='sequelize' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg" />
-                            <img alt='express' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
-                            <img alt='redux' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" />
-                            <img alt='github' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
-                            <img alt='bootstrap' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" />
-                            <img alt='figma' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" />
-                            <img alt='heroku' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original.svg" />
-                            <img alt='git' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
-                            <img alt='vscode' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" />
+                            <div className='devicon-div'>
+                                <img alt='react' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+                                <p>React</p>
+                            </div>
+                            <div className="devicon-div">
+                                <img alt='html' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+                                <p>HTML</p>
+                            </div>
+                            <div className="devicon-div">
+                                <img alt='css' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+                                <p>CSS</p>
+                            </div>
+                            <div className="devicon-div">
+                                <img alt='tailwind' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" />
+                                <p>Tailwind</p>
+                            </div>
+                            <div className="devicon-div">
+                                <img alt='javascript' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                                <p>Javascript</p>
+                            </div>
+                            <div className="devicon-div">
+                                <img alt='node' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" />
+                                <p>Node.js</p>
+                            </div>
+                            <div className="devicon-div">
+                                <img alt='postgres' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
+                                <p>Postgres</p>
+                            </div>
+                            <div className="devicon-div">
+                                <img alt='sequelize' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg" />
+                                <p>Sequelize</p>
+                            </div>
+                            <div className="devicon-div">
+                                <img alt='express' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
+                                <p>Express</p>
+                            </div>
+                            <div className="devicon-div">
+                                <img alt='redux' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" />
+                                <p>Redux</p>
+                            </div>
+                            <div className="devicon-div">
+                                <img alt='github' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
+                                <p>GitHub</p>
+                            </div>
+                            <div className="devicon-div">
+                                <img alt='bootstrap' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" />
+                                <p>Bootstrap</p>
+                            </div>
+                            <div className="devicon-div">
+                                <img alt='figma' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" />
+                                <p>Figma</p>
+                            </div>
+                            <div className="devicon-div">
+                                <img alt='heroku' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original.svg" />
+                                <p>Heroku</p>
+                            </div>
+                            <div className="devicon-div">
+                                <img alt='git' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
+                                <p>Git</p>
+                            </div>
+                            <div className="devicon-div">
+                                <img alt='vscode' className='tech-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" />
+                                <p>VSCode</p>
+                            </div>
                         </div>
                     </div>
                     <div className="education-div">
--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom'
 import './LandingPage.css'
 import React from 'react'
 
 const LandingPage = () => {
     return (
         <div className='home-computer-div'>
-            <img className='home-computer' alt='computer' src='https://drive.google.com/uc?export=view&id=1jfu0OHtWfi5s714et1YFlv_orY-D-yXr' />
+            <Link to='/about'>
+                <img className='home-computer' alt='computer' src='https://drive.google.com/uc?export=view&id=1jfu0OHtWfi5s714et1YFlv_orY-D-yXr' />
+            </Link>
         </div>
     )
 }
